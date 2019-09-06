f8b8d7e74d72795b289efb0b7f0a719c45af99e7
OBalandina <danaia275@mail.ru>
2019-09-06 13:23:33 +0500

    test2

---
+++ src/Lesson1/test3/MainClass.java
@@ -0,0 +1,11 @@
+package lesson1.test3;
+
+import org.junit.Test;
+
+public class MainClass {
+  String class_string = "Hello, world";
+
+  public void getClassString() {
+    System.out.println(this.class_string);
+  }
+}
---
+++ src/Lesson1/test3/MainClassTest.java
@@ -0,0 +1,16 @@
+package lesson1.test3;
+
+import org.junit.Test;
+
+public class MainClassTest {
+  @Test
+  public void testGetClassString(){
+    MainClass a = new MainClass();
+
+
+  }
+
+
+
+
+}
