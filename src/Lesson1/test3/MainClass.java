15a8b3a6c7618a3d65b88cda78406243b63b2f6e
OBalandina <danaia275@mail.ru>
2019-09-10 17:24:43 +0500

    test3

--- src/Lesson1/test3/MainClass.java
+++ src/Lesson1/test3/MainClass.java
@@ -3,9 +3,11 @@ package lesson1.test3;
 import org.junit.Test;
 
 public class MainClass {
-  String class_string = "Hello, world";
+  String  class_string = "hEllo, world";
 
-  public void getClassString() {
-    System.out.println(this.class_string);
+  public String getClassString() {
+//    System.out.println(this.class_string);
+    return this.class_string;
   }
+
 }
--- src/Lesson1/test3/MainClassTest.java
+++ src/Lesson1/test3/MainClassTest.java
@@ -1,16 +1,65 @@
 package lesson1.test3;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MainClassTest {
+  private String str;
+
   @Test
-  public void testGetClassString(){
+  public void testGetClassString() {
+
     MainClass a = new MainClass();
 
+    String s2 = a.getClassString().substring(0, 5);
+
+    if (s2.equals("Hello")) {
+      System.out.println("Hello, world");
+    } else {
+      System.out.println("Такого значения нет");
+    }
+    if (s2.equals("hello")) {
+      System.out.println("hello, world");
+    } else {
+      System.out.println("Такого значения нет");
+    }
+    Assert.assertTrue("Нет Hello и hello", s2.equals("Hello") | s2.equals("hello"));
 
   }
-
-
-
-
 }
+
+
+//    if (s2 != "Hello") {
+//     Assert.fail("Oшибка");
+//    }
+//    if (s2 != "hello"){
+//      Assert.fail("ошибка");
+//    }
+
+
+//    Assert.assertTrue("Нет Hello и hello","s2 != \"hello\"");
+
+    //   if (a.getClassString() == "Hello, world") {
+//    if (a.getClassString().substring(0, 5) == "Hello") {
+    //   }
+//      System.out.println(a.getClassString().substring(0, 5));
+//    }
+    //  else {
+    //     System.out.println("false");
+    //  }
+//  }
+
+//}
+
+
+//    if (a.getClassString() == "hello") {
+//      System.out.println("пусто");
+//    }
+//    else
+//    if (a.getClassString() == "Hello")
+//    {System.out.println("Hello,world");
+//    }
+
+
+//    if (a.getClassString()== "Hello")
+//      {System.out.println("Hello,world");}
